31626b140b5027feb1934a5095348d94fbf27793
askoenig <askoenig94@gmail.com>
2020-04-28 15:07:04 -0500

    squashed bugs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let num1;
-let num2;
-let clickedOperator;
+let num2 = false;
+let chosenOperator = false;
 let operating = false;
 let decimalUsed = false;
 
@@ -9,20 +9,25 @@ let input = document.querySelector('input[name="output"]');
 
 for (let i = 0; i < button.length; i++) {
   button[i].addEventListener("click", () => {
-    if (!!operating) {
+    if (button[i].innerHTML === "C") {
       input.value = "";
-      input.value = button[i].innerHTML;
+      num1 = "";
+      num2 = false;
+      chosenOperator = false;
+      decimalUsed = false;
+      operating = false;
+    } else if (!!operating) {
+      if (!!num2) {
+        input.value += button[i].innerHTML;
+      } else {
+        input.value = "";
+        input.value = button[i].innerHTML;
+        num2 = true;
+      }
     } else if (button[i].innerHTML === "." && !decimalUsed) {
       input.value = input.value + ".";
       decimalUsed = true;
       document.getElementById("decimal").disabled = true;
-    } else if (button[i].innerHTML === "C") {
-      input.value = "";
-      num1 = "";
-      num2 = "";
-      clickedOperator = false;
-      decimalUsed = false;
-      operating = false;
     } else {
       input.value += button[i].innerHTML;
     }
@@ -32,18 +37,19 @@ for (let i = 0; i < button.length; i++) {
 const operator = document.querySelectorAll(".operator-button");
 for (let i = 0; i < operator.length; i++) {
   operator[i].addEventListener("click", () => {
-    if (!!operating) {
-      console.log(num1);
+    if (!!operating && num2) {
       num2 = input.value;
-      operate(num1, num2, clickedOperator);
+      operate(num1, num2, chosenOperator);
       num1 = input.value;
-      clickedOperator = operator[i].innerHTML;
+      chosenOperator = operator[i].innerHTML;
       operating = true;
       decimalUsed = false;
+      num2 = false;
       document.getElementById("decimal").disabled = false;
     } else {
       num1 = input.value;
-      clickedOperator = operator[i].innerHTML;
+      num2 = false;
+      chosenOperator = operator[i].innerHTML;
       operating = true;
       decimalUsed = false;
       document.getElementById("decimal").disabled = false;
@@ -53,9 +59,12 @@ for (let i = 0; i < operator.length; i++) {
 
 const equals = document.querySelector(".equals-button");
 equals.addEventListener("click", () => {
-  num2 = input.value;
-  operate(num1, num2, clickedOperator, true);
-  operating = false;
+  if (!!operating && num2) {
+    num2 = input.value;
+    operate(num1, num2, chosenOperator);
+    operating = false;
+    num2 = false;
+  }
 });
 
 add = (x, y) => parseFloat(x) + parseFloat(y);
@@ -64,7 +73,15 @@ subtract = (x, y) => x - y;
 
 multiply = (x, y) => x * y;
 
-divide = (x, y) => x / y;
+divide = (x, y) => {
+  if (x === 0) {
+    return "0";
+  } else if (y == "0") {
+    return "Not a number!";
+  } else {
+    return x / y;
+  }
+};
 
 modulo = (x, y) => x % y;
 
@@ -80,19 +97,9 @@ function operate(num1, num2, operator) {
   } else if (operator === "%") {
     input.value = modulo(num1, num2);
   }
-
   num1 = input.value;
-  num2 = "";
+  num2 = false;
   decimalUsed = false;
   operating = false;
   document.getElementById("decimal").disabled = false;
 }
-
-const clearButton = document.getElementById("clear");
-clearButton.addEventListener("click", () => {
-  num1 = "";
-  num2 = "";
-  decimalUsed = false;
-  operating = false;
-  document.getElementById("decimal").disabled = false;
-});
